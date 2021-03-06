078cb692e16aa80da44ed2d8cc632ab6fbdcfe33
arronwelch <Davego@protonmail.com>
2021-03-06 23:33:36 +0800

    .

---
+++ led/src/main.c
@@ -0,0 +1,16 @@
+#include <REG52.H>
+
+sbit LED=P0^0;
+
+void main() {
+    LED=1;
+    while (1)
+    {
+        
+    }
+    
+}
+
+
+
+
---
+++ led_flashing/src/main.c
@@ -0,0 +1,48 @@
+#include <REG52.H>
+
+typedef unsigned char u8;
+typedef unsigned int u16;
+
+sbit led = P0 ^ 0;
+
+void Delay500ms() //@11.973MHz
+{
+    unsigned char i, j, k;
+
+    i = 23;
+    j = 192;
+    k = 86;
+    do
+    {
+        do
+        {
+            while (--k)
+                ;
+        } while (--j);
+    } while (--i);
+}
+
+void Delay1000ms() //@11.973MHz
+{
+    unsigned char i, j, k;
+
+    i = 46;
+    j = 127;
+    k = 177;
+    do
+    {
+        do
+        {
+            while (--k)
+                ;
+        } while (--j);
+    } while (--i);
+}
+
+void main()
+{
+    led = 1;
+    Delay1000ms();
+    led = 0;
+    Delay1000ms();
+}
